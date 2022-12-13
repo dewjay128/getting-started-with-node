4aedc766aad271b4da8de5d812c2a41fc8bf3a1b
Tommy Lee <cstlee32@gmail.com>
2022-12-13 15:44:39 -0500

    cors

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST;
 const db = require("./db");
 
 // App
 const app = express();
+
+app.use(cors());
 app.head("/", (req, res) => {
   return res.sendStatus(200);
 });

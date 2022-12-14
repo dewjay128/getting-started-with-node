9a424e635697ba1a7d71a1d19f08f95b10e981d1
Dong Woo Jung <stevejobscomputer@Dongs-MacBook-Pro.local>
2022-12-13 23:12:35 -0500

    users table

--- db.js
+++ db.js
@@ -20,4 +20,28 @@ const getSuggestions = (request, response) => {
   }
 };
 
-module.exports = { getSuggestions };
+const createUser = (request, response) => {
+  const { email, message } = request.body;
+
+  pool.query(
+    "INSERT INTO users (email, message) VALUES ($1, $2) RETURNING *",
+    [email, message],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    }
+  );
+};
+
+const getUsers = (request, response) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+module.exports = { getSuggestions, createUser, getUsers };
--- server.js
+++ server.js
@@ -3,13 +3,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST;
 const db = require("./db");
 
 // App
 const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.get("/", db.getSuggestions);
+app.get("/users", db.getUsers);
+app.post("/users", db.createUser);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
